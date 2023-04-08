692d55b8a288c9412aa6bc8365b30b22c4a926ff
oolufolabii <otitoola.olufolabi@gmail.com>
2023-04-08 15:53:38 +0100

    forms

--- all_forms.py
+++ all_forms.py
@@ -3,27 +3,32 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Email
 
+STYLE = {'class': 'ourClasses',
+         'style': 'margin: 1%; font-family: "DM Serif Display", serif; font-weight: 400;'}
+
 
 class RegistrationForm(FlaskForm):
     """Class for to create the registration form"""
-    style = {'class': 'ourClasses', 'style': 'margin: 1%; font-family: "DM Serif Display", serif; font-weight: 400;'}
+    username = StringField("Name", validators=[
+                           DataRequired()], render_kw=STYLE)
+    email = StringField(
+        "E-Mail", validators=[DataRequired(), Email()], render_kw=STYLE)
+    password = PasswordField("Password", validators=[
+                             DataRequired()], render_kw=STYLE)
+    submit = SubmitField("REGISTER", render_kw=STYLE)
 
-    username = StringField("Name", validators=[DataRequired()], render_kw=style)
-    email = StringField("E-Mail", validators=[DataRequired(), Email()], render_kw=style)
-    password = PasswordField("Password", validators=[DataRequired()], render_kw=style)
-    submit = SubmitField("REGISTER", render_kw=style)
-    
 
 class Login(FlaskForm):
     """Login info"""
-    style = {'class': 'ourClasses', 'style': 'margin: 1%; font-family: "DM Serif Display", serif; font-weight: 400;'}
-    email = StringField("Email", validators=[DataRequired(), Email()], render_kw=style)
-    password = PasswordField("Password", validators=[DataRequired()], render_kw=style)
-    submit = SubmitField("LOG IN!", render_kw=style)
+    email = StringField("Email", validators=[
+                        DataRequired(), Email()], render_kw=STYLE)
+    password = PasswordField("Password", validators=[
+                             DataRequired()], render_kw=STYLE)
+    submit = SubmitField("LOG IN!", render_kw=STYLE)
 
 
 class AddTag(FlaskForm):
     """Adding and creating new tags for tasks"""
-    style = {'class': 'ourClasses', 'style': 'margin: 1%; font-family: "DM Serif Display", serif; font-weight: 400;'}
-    tag_name = StringField("Tag Name", validators=[DataRequired()], render_kw=style)
-    submit = SubmitField("ADD TAG", render_kw=style)
+    tag_name = StringField("Tag Name", validators=[
+                           DataRequired()], render_kw=STYLE)
+    submit = SubmitField("ADD TAG", render_kw=STYLE)
