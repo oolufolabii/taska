ed73c3c6c35e63c6250dbc615e159d1d02ac7768
oolufolabii <otitoola.olufolabi@gmail.com>
2023-04-17 13:16:33 +0100

    Login, registration and tag forms for users

---
+++ app_forms.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Different forms for the flask app ` main.py `
+"""
+
+"""Module for flask forms"""
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email
+
+STYLE = {'class': 'ourClasses',
+         'style': 'margin: 1%; font-family: "DM Serif Display", serif; font-weight: 400;'}
+
+
+class RegistrationForm(FlaskForm):
+    """Class for to create the registration form"""
+    username = StringField("Name", validators=[
+                           DataRequired()], render_kw=STYLE)
+    email = StringField(
+        "E-Mail", validators=[DataRequired(), Email()], render_kw=STYLE)
+    password = PasswordField("Password", validators=[
+                             DataRequired()], render_kw=STYLE)
+    submit = SubmitField("REGISTER", render_kw=STYLE)
+
+
+class Login(FlaskForm):
+    """Login info"""
+    email = StringField("Email", validators=[
+                        DataRequired(), Email()], render_kw=STYLE)
+    password = PasswordField("Password", validators=[
+                             DataRequired()], render_kw=STYLE)
+    submit = SubmitField("LOG IN!", render_kw=STYLE)
+
+
+class AddTag(FlaskForm):
+    """Adding and creating new tags for tasks"""
+    tag_name = StringField("Tag Name", validators=[
+                           DataRequired()], render_kw=STYLE)
+    submit = SubmitField("ADD TAG", render_kw=STYLE)
